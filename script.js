bc6026a7ca8555dda5f0c900e32742e69de9deca
sonja-ops <sodju@hotmail.com>
2024-10-24 11:32:34 +0200

    legget noe mer

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ console.log('Hello!');
 
 // let variable saving the number 48 on the name age
 let age=48;
-console. log(age);
+// Chaining method:
+console. log("Min alder er", age)
+//  Templite
 console.log(tipeofage);
-
 // const varisble saving the string Sonja on the name name
 const name="Sonja"
-console.log(name);
+console.log(" mitt navn er:", name);
 console.log(typeof name);
 
 // Const variable saving the boolesn true on the name isPresent
 const isPresent = true
-console.log(isPresent);
+console.log("ER tilstedet", isPresent);
 console.log(typeof isPresent);
+
+const firstName = "Sonja";
+
+const lastName = "Stefanovic";
+
+const fullname = firstName + " " + lastName;
+console.log(fullname);
