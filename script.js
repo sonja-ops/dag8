c69eb1c19b305e5ded92608c1cf64dc9a7a9b97f
sonja-ops <sodju@hotmail.com>
2024-10-24 10:52:27 +0200

    slettet noe

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello!');
+
+// Variables
+
+// let variable saving the number 48 on the name age
+let age=48;
+console. log(age);
+console.log(tipeofage);
+
+// const varisble saving the string Sonja on the name name
+const name="Sonja"
+console.log(name);
+console.log(typeof name);
+
+// Const variable saving the boolesn true on the name isPresent
+const isPresent = true
+console.log(isPresent);
+console.log(typeof isPresent);
